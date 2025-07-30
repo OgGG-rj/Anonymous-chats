2b007801921d61745eb49c2dfbce196c410287aa
OgGG-rj <ggwithrj@gmail.com>
2025-07-30 13:05:04 +0530

    server.js

--- server.js
+++ server.js
@@ -12,9 +12,11 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('A user connected');
+
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+    io.emit('chat message', msg);  // 🔥 This shares it with everyone
   });
+
   socket.on('disconnect', () => {
     console.log('A user disconnected');
   });
